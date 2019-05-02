970b5df1cd4493682479eaacb4afc176728b1c79
John Barrie <john.barrie@kyan.com>
2019-05-03 01:51:24 +0200

    foundation test

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './service_worker';
+import './index.scss';
+
 import App from './app';
 
-import './index.scss';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
--- src/view/partial/top_nav.js
+++ src/view/partial/top_nav.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './top_nav.scss';
 
 class TopNav extends React.Component {
   render () {
